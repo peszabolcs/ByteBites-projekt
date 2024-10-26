570d189e64d97dd6c213f8e58cb4a0543532df8e
peszabolcs <perjesidev@gmail.com>
2024-10-26 17:43:56 +0200

    Maven hozzáadása

---
+++ src/main/java/hu/university/etelprojekt/etelprojekt/EtelProjektApplication.java
@@ -0,0 +1,13 @@
+package hu.university.etelprojekt.etelprojekt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtelProjektApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtelProjektApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/university/etelprojekt/etelprojekt/EtelProjektApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.university.etelprojekt.etelprojekt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtelProjektApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
