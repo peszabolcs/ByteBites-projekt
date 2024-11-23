b29ef788da8988f346fa77654cc86ed3a482ae95
h376113 <h376113@stud.u-szeged.hu>
2024-11-23 10:55:47 +0100

    Changes

--- src/main/java/hu/university/etelprojekt/etelprojekt/entity/Address.java
+++ src/main/java/hu/university/etelprojekt/etelprojekt/entity/Address.java
@@ -1,66 +1,40 @@
 package hu.university.etelprojekt.etelprojekt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.List;
 
+@Getter
+@Setter
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "address") // Optional: specify the table name in the database
+//@Table(name = "address") // Optional: specify the table name in the database
 public class Address implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
     @Id
-    @Setter
-    @Getter
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generate the addressId (primary key)
     private Long address_id;
-
-    @Setter
-    @Getter
     private String postalCode;
-
-    @Setter
-    @Getter
     private String city;
-
-    @Setter
-    @Getter
     private String streetName;
-
-    @Setter
-    @Getter
     private String publicPlaceName;
-
-    @Setter
-    @Getter
     private String houseNumber;
-
-    @Setter
-    @Getter
     private String building;
-
-    @Setter
-    @Getter
     private String staircase;
-
-    @Setter
-    @Getter
     private String floor;
-
-    @Setter
-    @Getter
     private String door;
+
+//    @OneToMany
+//    @JoinColumn(name = "restaurant_id")
+//    private List<Restaurant> restaurants;
 
     @Override
     public String toString() {
--- src/main/java/hu/university/etelprojekt/etelprojekt/entity/Allergen.java
+++ src/main/java/hu/university/etelprojekt/etelprojekt/entity/Allergen.java
@@ -1,50 +1,31 @@
 package hu.university.etelprojekt.etelprojekt.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import java.io.Serializable;
+import java.util.List;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "allergens") // Optional: specify the table name in the database
 public class Allergen implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Setter
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Automatically generate the allergenId (primary key)
     private Long allergenId;
+    private String allergenName;
 
-    private String allergenName; // Store the allergen name directly as a String
-
-    // Default constructor
-    public Allergen() {
-    }
-
-    // Constructor with all fields
-    public Allergen(Long allergenId, String allergenName) {
-        this.allergenId = allergenId;
-        this.allergenName = allergenName;
-    }
-
-    // Getters and Setters
-    public Long getAllergenId() {
-        return allergenId;
-    }
-
-    public void setAllergenId(Long allergenId) {
-        this.allergenId = allergenId;
-    }
-
-    public String getAllergenName() {
-        return allergenName;
-    }
-
-    public void setAllergenName(String allergenName) {
-        this.allergenName = allergenName;
-    }
+    @OneToMany
+    @JoinColumn(name = "restaurant_id")
+    private List<Restaurant> restaurants;
 
     @Override
     public String toString() {
